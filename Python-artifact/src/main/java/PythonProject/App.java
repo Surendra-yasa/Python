804499bcc9870d66f1231438bbab3477eede899b
Surendra Kumar <y.surendra9912@gmail.com>
2022-08-04 14:49:11 +0530

    python

---
+++ Python-artifact/src/main/java/PythonProject/App.java
@@ -0,0 +1,13 @@
+package PythonProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
